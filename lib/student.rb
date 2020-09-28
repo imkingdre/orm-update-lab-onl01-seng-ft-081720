c4f0fdd6d03c049de711218757a9c2399dd3e299
Andre King <andreking1031@gmail.com>
2020-09-28 20:56:52 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -33,6 +33,7 @@ class Student
     VALUES (?, ?)
     SQL
     DB[:conn].execute(sql, self.name, self.grade)
+    @id = id
   end
 
   # Remember, you can access your database connection anywhere in this class
