ea3819a578e6e9b0037a6cecff8c75fed90501dc
Andre King <andreking1031@gmail.com>
2020-09-28 21:05:15 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -28,7 +28,8 @@ class Student
   end 
   
   def save
-    self.id
+    if self.id
+      self.update
     DB[:conn].execute(sql, self.name, self.grade)
     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
   end
