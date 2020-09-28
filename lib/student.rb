0cb5ce003df696d6083d58cc85c483490773bafa
Andre King <andreking1031@gmail.com>
2020-09-28 20:54:15 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -26,6 +26,10 @@ class Student
   def self.drop_table
     
   end 
+  
+  def save
+    
+  end
 
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
